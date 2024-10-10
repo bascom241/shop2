e9ab4c39c365fe4db266790bdd1994b1b604a2c9
bascom241 <135514268+bascom241@users.noreply.github.com>
2024-10-10 23:05:45 +0100

    Update Inventory.js

--- myShop/admin/src/pages/Inventory.js
+++ myShop/admin/src/pages/Inventory.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import './Inventory.css';
 import axios from 'axios';
 import { toast } from 'react-toastify';
